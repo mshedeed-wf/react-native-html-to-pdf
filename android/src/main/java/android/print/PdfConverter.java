30505aa16ede285a18e6922426b9a6f199c7b844
mshedeed-wf <shedeeed@gmail.com>
2024-07-24 18:40:25 +0300

    [feat] change page type to ISO A3

--- android/src/main/java/android/print/PdfConverter.java
+++ android/src/main/java/android/print/PdfConverter.java
@@ -157,7 +157,7 @@ public class PdfConverter implements Runnable {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) return null;
 
         return new PrintAttributes.Builder ()
-                .setMediaSize (PrintAttributes.MediaSize.NA_LETTER)
+                .setMediaSize (PrintAttributes.MediaSize.ISO_A3)
                 .setResolution (new PrintAttributes.Resolution ("RESOLUTION_ID", "RESOLUTION_ID", 600, 600))
                 .setMinMargins (PrintAttributes.Margins.NO_MARGINS)
                 .build ();
